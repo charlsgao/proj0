cbef0cea17408d40573fe105ff7552860cfdf9ee
saasbook <saasbook@saasbook.(none)>
2013-02-15 09:15:33 -0800

    pro

--- warmup_apps/tests.py
+++ warmup_apps/tests.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append('/home/ubuntu/warmup_proj/')
+sys.path.append('/salty-castle-4218.herokuapp.com/')
 #sys.path.append('/home/ubuntu/')
 os.environ['DJANGO_SETTINGS_MODULE'] = "warmup_proj.settings"
 
